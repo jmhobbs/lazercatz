6701ba81f4dcaab253207cba85d7444f0b9e8f76
Jerod Santo <jerod.santo@gmail.com>
2010-08-28 12:11:50 -0500

    allow configurable port via gitignored config.json

--- server.js
+++ server.js
@@ -2,4 +2,7 @@ require.paths.unshift(__dirname + "/vendor");
 
 var LazerCatz = require('./lib/lazercatz');
 
-new LazerCatz({});
+require('fs').readFile('config.json', function (err, data) {
+    if (err) throw err;
+    new LazerCatz(JSON.parse(data));
+});
