a542c0e482702a65757fe80b86a9137a3667e5fd
John Hobbs <john@velvetcache.org>
2010-08-27 21:33:42 -0500

    Get a handle on our elements.

--- static/lazercatz.js
+++ static/lazercatz.js
@@ -1,3 +1,12 @@
 var LC = {
+	ctx: null,
+	map: null,
+	sprites: null,
+	sprite_map: {}
 
+	init: function () {
+		LC.ctx = document.getElementById( "objects" ).getContext( "2d" );
+		LC.map = $( "#map" );
+		LC.sprites = document.getElementById( "sprites" );
+	}
 }
