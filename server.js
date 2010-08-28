6bcca9499f894f5ea449de4390319507cf5f86d2
Jerod Santo <jerod.santo@gmail.com>
2010-08-28 12:21:37 -0500

    default to port 80 if there is no config.json in the repo

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ require.paths.unshift(__dirname + "/vendor");
 var LazerCatz = require('./lib/lazercatz');
 
 require('fs').readFile('config.json', function (err, data) {
-    if (err) throw err;
-    new LazerCatz(JSON.parse(data));
+    if (err) {
+        new LazerCatz({});
+    } else {
+        new LazerCatz(JSON.parse(data));
+    }
 });
